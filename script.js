da90c25efa41799f2dc558866f60ac62806a76a4
Rio-Kartoffel <riokartoffel09@gmail.com>
2023-01-19 17:48:38 +0800

    edit

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const panels = document.querySelectorAll('.panel')
 
-panels.forEach((panel) => {
-    panel.addEventListener('click, () => 
-    {
+panels.forEach((panel) =>{
+    panel.addEventListener('click', () =>{
         removeActiveClasses()
         panel.classList.add('active')
     })
-
+})
 
 function removeActiveClasses() {
     panels.forEach(panel => {
